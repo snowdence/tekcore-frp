8ebade02802f24bdbce1be09820b65200fff6a2a
Đức Trần <snowdence2911@gmail.com>
2023-10-12 11:27:52 +0700

    up

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,34 +8,35 @@ MASTER_NAME=$1
 
 USAGE=$(cat <<-END
 [common]
-server_addr = t.wifosoft.com
-#server_addr = frp.tekcore.win
-server_port = 7000
+server_addr=t.wifosoft.com
+#server_addr=frp.tekcore.win
+server_port=7000
 
 [ssh]
-type = tcp
-local_ip = 127.0.0.1
-local_port = 22
-remote_port = 6000
+type=tcp
+local_ip=127.0.0.1
+local_port=22
+remote_port=6000
 
 
 [${MASTER_NAME}-master]
-type = http
-subdomain = ${MASTER_NAME}-8001
-local_port = 8001
+type=http
+subdomain=${MASTER_NAME}-master
+local_port=8001
 
 [${MASTER_NAME}-acb-6554]
-type = http
-subdomain = ${MASTER_NAME}-acb-6554
-local_port = 6554
+type=http
+subdomain=${MASTER_NAME}-acb-6554
+local_port=6554
 
 [${MASTER_NAME}-acb-6556]
-type = http
-subdomain = ${MASTER_NAME}-acb-6556
-local_port = 6556
+type=http
+subdomain=${MASTER_NAME}-acb-6556
+local_port=6556
 END
 )
 
 echo $USAGE
 
 echo "$USAGE" > ./frp_0.51.4_linux_amd64/frpc.ini
+./frp_0.51.4_linux_amd64/frpc -c ./frp_0.51.4_linux_amd64/frpc.ini
